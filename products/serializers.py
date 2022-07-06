8ea7db593a6da64cfaaf823c83fa0f6b87038ca0
eshaanmangal <eshaan.m@media.net>
2022-07-06 12:41:24 +0530

    Adding Product Transaction Serializer

--- products/serializers.py
+++ products/serializers.py
@@ -16,6 +16,23 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductTranscationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ProductTransaction
         fields = '__all__'
+    
+    # This doesn't return error gracefully
+    def validate_amount(self, amount):
+        if amount <= 0:
+            return serializers.ValidationError("Amount should be greater than 0")
+        return amount
+    
+    # have to complete it
+    def validate(self, attrs):
+        if attrs['direction'] == 'OUT':
+            name = attrs['name'].strip()
+            qs = ProductTransaction.objects.all().filter(direction__exact='IN', name__exact=name, product_id=attrs['product_id'])
+            if qs.exists():
+                if qs[0].amount < attrs['amount']:
+                    return serializers.ValidationError("OUT amount cannot be greater than IN amount")
+        return super().validate(attrs)
